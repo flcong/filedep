4b0d724e6a882a707f33143dee4f0481c98d866a
Francis Cong <flcong@outlook.com>
2021-10-14 14:44:17 -0400

    Make ready to upload to PyPI

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
       version='0.1',
       description='filedep: A small python tool to check file dependency',
       author='Francis Cong',
+      author_email='flcong@outlook.com',
       url='https://github.com/flcong/filedep',
       license='MIT',
       packages=['filedep'],
