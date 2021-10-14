075ca872ed3718c003635f4619beb914eeea5eac
Francis Cong <flcong@outlook.com>
2021-10-14 14:57:42 -0400

    Change setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup
 
 setup(
-      name='filedep',
-      version='0.1',
-      description='filedep: A small python tool to check file dependency',
-      author='Francis Cong',
-      author_email='flcong@outlook.com',
-      url='https://github.com/flcong/filedep',
       license='MIT',
       packages=['filedep'],
       zip_safe=False
